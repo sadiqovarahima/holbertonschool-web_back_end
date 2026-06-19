5d89c4481895505a2086c939dfe7f87aeece1454
sadiqovarahima <13726@holbertonstudents.com>
2026-06-19 11:18:45 +0200

    change task

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
-'''Async Comprehension'''
+""" Async comprenhesion gen """
+import asyncio
 from typing import List
+
+
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Async comprehension'''
-    return [i async for i in async_generator()]
+    """
+        Generate numbers with async comprenhension
+
+        Args:
+            void
+
+        Return:
+            float random numbers
+    """
+    return ([i async for i in async_generator()])
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
-'''Async Comprehension'''
+""" Measure times """
 import asyncio
-from typing import List
+import time
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''Measure runtime'''
-    start = asyncio.get_event_loop().time()
-    await asyncio.gather(*[async_comprehension() for _ in range(4)])
-    end = asyncio.get_event_loop().time()
-    return end - start
+    """
+        measure time and execute in paralallel
+
+        Args:
+            void
+
+        Return:
+            float random numbers
+    """
+    first_time = time.perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    elapsed = time.perf_counter()
+
+    return (elapsed - first_time)
