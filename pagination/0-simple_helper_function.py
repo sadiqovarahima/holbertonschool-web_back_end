664abab4b8c4c078b80e3fbadb4d16cb614ab46b
sadiqovarahima <13726@holbertonstudents.com>
2026-07-19 19:02:58 +0000

    fix task

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,5 +8,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """funksiya"""
     start_index = (page - 1) * page_size
-    end_index = page * page_size 
+    end_index = page * page_size
     return (start_index, end_index)
