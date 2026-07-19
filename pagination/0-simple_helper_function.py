a3831a4f4fc2ad322add295a137a6c3cc51cb88b
sadiqovarahima <13726@holbertonstudents.com>
2026-07-19 16:57:27 +0000

    fix

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Səhifələmə (Pagination) üçün köməkçi funksiya.
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Səhifə nömrəsi və ölçüsünə əsasən başlanğıc və 
+    son indeksləri qaytaran funksiya.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    
+    return (start_index, end_index)
